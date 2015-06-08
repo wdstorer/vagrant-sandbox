f839337b94d8fcaba32644284f4bd931061c873b
Will <wdstorer@yahoo.com>
2015-06-07 21:02:06 -0400

    Adding mongodb test machine

--- mongodb-repl01/manifests/mongodbreplset-test01.pp
+++ mongodb-repl01/manifests/mongodbreplset-test01.pp
@@ -1,16 +1,12 @@
-file { "/customer/data/mongodb-data":
-  ensure => "directory",
-}->
-file { "/customer/data/mongodb-log":
-  ensure => "directory",
+file {['/customer','/customer/data']:
+  ensure => directory,
 }->
 class {'::mongodb::globals':
   manage_package_repo => true,
 }->
-class {'::mongodb::server':
+class {'::mongodb::server': 
   replset => test01,
-  dbpath => "/customer/data/mongodb-data",
-  logpath => "/customer/data/mongodb-log/mongodb.log",
-}->
+  dbpath => '/customer/data/mongodb-data',
+}
 class {'::mongodb::client': }
 
